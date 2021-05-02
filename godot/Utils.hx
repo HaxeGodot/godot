4b79ab26733f92dcf5b38b4c925a05b45b0c73d1
Valentin Lemière <valentin@ibilon.com>
2021-05-02 22:43:44 +0200

    Use native as

--- godot/Utils.hx
+++ godot/Utils.hx
@@ -1,18 +1,7 @@
 package godot;
 
 class Utils {
-	public static function as<T>(value:godot.Object, type:Class<T>):T {
-		var cls:Class<Any> = Type.getClass(value);
-		final type = Type.getClassName(type);
-
-		while (cls != null) {
-			if (Type.getClassName(cls) == type) {
-				return cast value;
-			} else {
-				cls = Type.getSuperClass(cls);
-			}
-		}
-
-		return null;
+	public static inline function as<T>(obj:godot.Object, type:Class<T>):T {
+		return cs.Lib.as(obj, type);
 	}
 }
